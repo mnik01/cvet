f19c2f874026bd11e0b03fc9e5b800e8392ba643
mnik01 <maksimgrinberg01@gmail.com>
2022-08-25 15:14:24 +0600

    feat: map toggle

--- src/App.tsx
+++ src/App.tsx
@@ -154,8 +154,14 @@ export default function App() {
             <a className="grow pb-2" href="https://go.2gis.com/ui5tl">
               <div
                 style={{ backgroundImage: `url(${mapUrl})` }}
-                className="h-full cover bg-no-repeat rounded-lg bg-center"
-              ></div>
+                className="h-full relative cover bg-no-repeat rounded-lg bg-center"
+              >
+                <div className="absolute right-0 top-0 bg-ui-white p-2 flex items-center justify-center m-2 rounded-lg text-ui-black">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.6} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+                  </svg>
+                </div>
+              </div>
             </a>
             <p className="text-sm text-ui-gray">Сибирский тракт, 24г</p>
           </div>
